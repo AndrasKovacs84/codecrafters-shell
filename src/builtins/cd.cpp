50a7272a4b4443c6cd900dc551b15e341ea54373
Andras Kovacs <ka841001@gmail.com>
2025-09-12 21:16:21 +0200

    tilde replace with home var in cd

--- src/builtins/cd.cpp
+++ src/builtins/cd.cpp
@@ -1,6 +1,9 @@
 #include "cd.hpp"
+#include "../shell/shell.hpp"
 
+#include <algorithm>
 #include <filesystem>
+#include <forward_list>
 
 CdCommand::CdCommand(std::string_view cmd)
     : m_Command(cmd)
@@ -14,9 +17,20 @@ auto CdCommand::Execute() -> bool
     size_t pos = m_Command.find(' ');
     if (pos == std::string::npos)
     {
-        return false;
+        return true;
     }
-    std::string_view args{m_Command.begin() + pos + 1, m_Command.end()};
+    std::string args{m_Command.substr(pos + 1)};
+
+    while ((pos = args.find('~')) != std::string::npos)
+    {
+        std::optional<std::string> home = Shell::GetEnv("HOME");
+        if (home.has_value())
+        {
+            std::forward_list<char> home_container;
+            std::ranges::reverse_copy(home.value(), std::front_inserter(home_container));
+            args.replace_with_range(args.begin() + pos, args.begin() + pos + 1, home_container);
+        }
+    }
 
     std::error_code ec;
     std::filesystem::current_path(std::filesystem::path(args), ec);
